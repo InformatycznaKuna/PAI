609008a83594746158d096f82aeb15a3daf04abe
InformatycznaKuna <piotrcoolxd@gmail.com>
2022-09-13 11:02:37 +0200

    Updated readme

---
+++ lekcja1/123.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css">
+	<title>Nowacki</title>
+</head>
+<body>
+<?php
+$zmienna = 2;
+   echo "Hello World <br> ";
+   echo "My counter $zmienna <br> ";
+   echo "Hello World ";
+?>
+</body>
+</html>
